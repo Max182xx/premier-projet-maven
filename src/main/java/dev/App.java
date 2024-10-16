e49a499f7f0864a7e8b1c8b435b0b5981625d987
Max182xx <m.garciapro34@gmail.com>
2024-10-16 17:01:37 +0200

    TP termniné

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -4,10 +4,11 @@ package dev;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App {
+    public static void main(String[] args) {
+        String asciiArt = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt);
     }
 }
